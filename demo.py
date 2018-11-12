7b9d1e258810ac0a8c0257aa35a2adec3a6b8558
Donghee Han <hdh7485@gmail.com>
2018-11-12 15:20:41 +0900

    Made CNN.

---
+++ demo.py
@@ -0,0 +1,128 @@
+import argparse
+import extractData_hdh
+import tensorflow as tf
+import numpy as np
+
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+class Model:
+    def __init__(self, sess, name):
+        self.sess = sess
+        self.name = name
+        self._build_net()
+
+    def _build_net(self):
+        with tf.variable_scope(self.name):
+            # dropout (keep_prob) rate  0.7~0.5 on training, but should be 1
+            # for testing
+            self.keep_prob = tf.placeholder(tf.float32)
+
+            self.X_img = tf.placeholder(tf.float32, [None, 5, 24])
+            self.X_img = tf.reshape(self.X_img, [-1, 5, 24, 1])
+            self.Y = tf.placeholder(tf.float32, [None, 14])
+
+            W1 = tf.Variable(tf.random_normal([2, 2, 1, 10], stddev=0.01))
+            L1 = tf.nn.conv2d(self.X_img, W1, strides=[1, 1, 1, 1], padding='SAME')
+            L1 = tf.nn.relu(L1)
+            L1 = tf.nn.dropout(L1, keep_prob=self.keep_prob)
+            #L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+            #5 24 5
+
+            W2 = tf.Variable(tf.random_normal([2, 2, 10, 50], stddev=0.01))
+            L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
+            L2 = tf.nn.relu(L2)
+            L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1],
+                                strides=[1, 2, 2, 1], padding='SAME')
+            L2 = tf.nn.dropout(L2, keep_prob=self.keep_prob)
+            # L2 ImgIn shape=(?, 3, 12, 50)
+            L2_flat = tf.reshape(L2, [-1, 50 * 3 * 12])
+
+
+            W4 = tf.get_variable("W4", shape=[50 * 3 * 12, 400],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+            b4 = tf.Variable(tf.random_normal([400]))
+            L4 = tf.nn.relu(tf.matmul(L2_flat, W4) + b4)
+            L4 = tf.nn.dropout(L4, keep_prob=self.keep_prob)
+
+            # L5 Final FC 400 inputs -> 14 outputs
+            W5 = tf.get_variable("W5", shape=[400, 14],
+                                 initializer=tf.contrib.layers.xavier_initializer())
+            b5 = tf.Variable(tf.random_normal([14]))
+            self.logits = tf.matmul(L4, W5) + b5
+
+        # define cost/loss & optimizer
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(
+            logits=self.logits, labels=self.Y))
+        self.optimizer = tf.train.AdamOptimizer(
+            learning_rate=learning_rate).minimize(self.cost)
+
+        correct_prediction = tf.equal(
+            tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def predict(self, x_test, keep_prop=1.0):
+        return self.sess.run(self.logits, feed_dict={self.X_img: x_test, self.keep_prob: keep_prop})
+
+    def get_accuracy(self, x_test, y_test, keep_prop=1.0):
+        return self.sess.run(self.accuracy, feed_dict={self.X_img: x_test, self.Y: y_test, self.keep_prob: keep_prop})
+
+    def train(self, x_data, y_data, keep_prop=0.7):
+        return self.sess.run([self.cost, self.optimizer], feed_dict={
+            self.X_img: x_data, self.Y: y_data, self.keep_prob: keep_prop})
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_directory", default="../raw_data/")
+    args = parser.parse_args()
+
+    data = extractData_hdh.IOTDataset()
+    data.load_json_files(args.data_directory)
+    beacon_table = data.make_time_onehot_beacon_table()
+    target_table = data.make_time_onehot_target_table()
+
+    print(beacon_table)
+    #(237017, 25)
+    print(target_table)
+    #(237017, 15)
+
+    beacon_split_table = beacon_table[0:237000, 1:].reshape(47400, 5, 24)
+    beacon_train, beacon_test = beacon_split_table[:40000, :, :], beacon_split_table[40000:, :, :]
+    target_split_table = target_table[0:237000, 1:].reshape(47400, 5, 14)
+    target_train, target_test = target_split_table[:40000, :, :], beacon_split_table[40000:, :, :]
+
+    print(beacon_split_table.shape)
+    print(target_split_table.shape)
+
+    # initialize
+    sess = tf.Session()
+    m1 = Model(sess, "m1")
+
+    sess.run(tf.global_variables_initializer())
+
+    print('Learning Started!')
+
+    # train my model
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(47400 / batch_size)
+
+        for i in range(total_batch):
+            #batch_xs = beacon_split_table[i*batch_size:(i+1)*batch_size, :, :, np.newaxis]
+            #batch_ys = target_split_table[i*batch_size:(i+1)*batch_size, -1, :]
+            batch_xs = beacon_train[i*batch_size:(i+1)*batch_size, :, :, np.newaxis]
+            batch_ys = target_train[i*batch_size:(i+1)*batch_size, -1, :]
+            c, _ = m1.train(batch_xs, batch_ys)
+            avg_cost += c / total_batch
+
+        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost))
+
+    print('Learning Finished!')
+
+    # Test model and check accuracy
+    # print('Accuracy:', m1.get_accuracy(beacon_test, target_test))
+
+
+if __name__ == "__main__":
+    main()
