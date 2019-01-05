e7308bd0c0fd9ff87f09c19a0884226b8c6a7114
Donghee Han <hdh7485@gmail.com>
2019-01-05 21:06:25 +0900

    Modify model size

--- iot_fcn.py
+++ iot_fcn.py
@@ -22,6 +22,70 @@ class Model:
             self.keep_prob = tf.placeholder(tf.float32)
             self.phase= tf.placeholder(tf.bool)
             self.X_img = tf.placeholder(tf.float32, [None, 4, 24, 1])
+            self.X = tf.reshape(self.X_img, [-1, 4 * 24])
+            self.negative_X = tf.negative(self.X)
+            self.Y = tf.placeholder(tf.float32, [None, 14])
+            
+            with tf.name_scope("fully_connected1"):
+                FC_W1 = tf.get_variable("FC_W1", shape=[4 * 24, 16],
+                                     initializer=tf.contrib.layers.xavier_initializer())
+                FC_b1 = tf.get_variable("FC_b1", shape=[16])
+                FC_L1 = tf.matmul(self.negative_X, FC_W1) + FC_b1
+                FC_L1 = tf.contrib.layers.batch_norm(FC_L1, center=True, scale=True, is_training=self.phase, scope='FC_bn1')
+                FC_L1 = tf.nn.relu(FC_L1)
+                FC_L1 = tf.nn.dropout(FC_L1, keep_prob=self.keep_prob)
+                # 400
+                self.FC_W1_hist = tf.summary.histogram("weights_FC1", FC_W1)
+                self.FC_b1_hist = tf.summary.histogram("bias_FC1", FC_b1)
+
+            with tf.name_scope("fully_connected3"):
+                # Final FC 400 inputs -> 14 outputs
+                FC_W3 = tf.get_variable("FC_W3", shape=[16, 14],
+                                     initializer=tf.contrib.layers.xavier_initializer())
+                FC_b3 = tf.get_variable("FC_b3", shape=[14])
+                self.logits = tf.matmul(FC_L1, FC_W3) + FC_b3
+                self.FC_W3_hist = tf.summary.histogram("weights_FC3", FC_W3)
+                self.FC_b3_hist = tf.summary.histogram("bias_FC3", FC_b3)
+
+        # define cost/loss & optimizer
+        with tf.name_scope("cost"):
+            self.softmax = tf.nn.softmax_cross_entropy_with_logits_v2(
+                logits=self.logits, labels=self.Y)
+            self.cost = tf.reduce_mean(self.softmax) 
+            self.cost_summ = tf.summary.scalar("cost", self.cost)
+
+        self.optimizer = tf.train.AdamOptimizer(
+            learning_rate=self.learning_rate).minimize(self.cost)
+        correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        self.accuracy_sum = tf.summary.scalar("accuracy", self.accuracy)
+        self.merged_summary = tf.summary.merge_all()
+
+
+    def predict(self, x_test, keep_prop=1.0):
+        return self.sess.run(self.logits, feed_dict={self.X_img: x_test, self.keep_prob: keep_prop, self.phase:False})
+
+    def get_accuracy(self, x_test, y_test, keep_prop=1.0):
+        return self.sess.run([self.accuracy, self.negative_X, self.Y, self.logits], feed_dict={
+            self.X_img: x_test, self.Y: y_test, self.keep_prob: keep_prop, self.phase:True})
+
+    def train(self, x_data, y_data, keep_prop=0.7):
+        return self.sess.run([self.merged_summary, self.cost, self.optimizer, self.accuracy], feed_dict={
+            self.X_img: x_data, self.Y: y_data, self.keep_prob: keep_prop, self.phase:True})
+class Model2:
+    def __init__(self, sess, name, learning_rate):
+        self.sess = sess
+        self.name = name
+        self.learning_rate = learning_rate
+        self._build_net()
+
+    def _build_net(self):
+        with tf.variable_scope(self.name):
+            # dropout (keep_prob) rate  0.7~0.5 on training, but should be 1
+            # for testing
+            self.keep_prob = tf.placeholder(tf.float32)
+            self.phase= tf.placeholder(tf.bool)
+            self.X_img = tf.placeholder(tf.float32, [None, 4, 24, 1])
             self.negative_X = tf.negative(self.X_img)
             self.Y = tf.placeholder(tf.float32, [None, 14])
             
@@ -107,8 +171,8 @@ def main():
     target_split_table = data.expand_time_onehot_beacon_table(target_table, 4)[:, -1, 1:]
     number_of_target = target_split_table.astype(np.int).sum(axis=0)
     print(number_of_target)
-    plt.plot(number_of_target)
-    plt.show()
+    #plt.plot(number_of_target)
+    #plt.show()
 
     # shuffle dataset
     idx = np.random.permutation(len(beacon_split_table))
@@ -122,21 +186,20 @@ def main():
         beacon_split_table, target_split_table, test_size=0.3, shuffle=False)
     beacon_valid, beacon_test, target_valid, target_test = train_test_split(
         beacon_test, target_test, test_size=0.5, shuffle=False)
-    '''
     # check imported data
-    for k in range(10):
+    '''
+    for k in range(100):
         fig = plt.figure()
         plt.subplot(211)
-        X = np.negative(np.reshape(beacon_valid[k], (4, 24)))
+        X = np.negative(np.reshape(beacon_valid[-k*4], (4, 24)))
         print(X)
         plt.imshow(X, cmap="gray")
         plt.subplot(212)
-        Y = target_valid[k]
+        Y = target_valid[-k*4]
         print(Y)
         plt.imshow(np.expand_dims(Y, axis=0), cmap="gray")
-        plt.savefig('img/valid_first_{}.png'.format(k), bbox_inches='tight')
-        #plt.show()    
-
+        #plt.savefig('img/valid_first_{}.png'.format(k), bbox_inches='tight')
+        plt.show()    
     for k in range(10):
         fig = plt.figure()
         plt.subplot(211)
@@ -162,24 +225,26 @@ def main():
         plt.imshow(np.expand_dims(Y, axis=0), cmap="gray")
         plt.savefig('img/train_first_{}.png'.format(k), bbox_inches='tight')
         #plt.show()    
-
-    for k in range(10):
+    '''
+    '''
+    for k in range(100):
         fig = plt.figure()
         plt.subplot(211)
-        X = np.negative(np.reshape(beacon_train[-k], (4, 24)))
+        X = np.negative(np.reshape(beacon_test[-k*4], (4, 24)))
         print(X)
         plt.imshow(X, cmap="gray")
         plt.subplot(212)
-        Y = target_train[-k]
+        Y = target_test[-k*4]
         print(Y)
         plt.imshow(np.expand_dims(Y, axis=0), cmap="gray")
-        plt.savefig('img/train_last_{}.png'.format(k), bbox_inches='tight')
-        #plt.show()    
-    '''    
+        #plt.savefig('img/train_last_{}.png'.format(k), bbox_inches='tight')
+        plt.show()    
+    '''
+
     # initialize
     sess = tf.Session()
 
-    m1 = Model(sess, "m1", 0.00001)
+    m1 = Model(sess, "m1", 0.0001)
     writer = tf.summary.FileWriter("./logs/iot_r0_02")
     writer.add_graph(m1.sess.graph)
     saver = tf.train.Saver()
